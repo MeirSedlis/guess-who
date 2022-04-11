c4ad322491ea1a2c3f60e3602867a8f822ebf3f7
Meir Sedlis <meirsedlis@gmail.com>
2022-04-11 16:29:32 -0500

    initial commit

--- client.js
+++ client.js
@@ -1,1 +1,17 @@
+$(document).ready(onReady);
+
 console.log('Here are all the available people:', people);
+// here is the url for chris https://avatars.githubusercontent.com/u/95505985?v=4
+// here is the url for meir https://avatars.githubusercontent.com/u/96435697?v=4
+
+function onReady(){
+    appendDivs();
+}
+
+function appendDivs(){
+    for (let person of people){
+        $('main').append(`
+        <img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris">
+        `)
+    }
+}
